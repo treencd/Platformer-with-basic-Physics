cf4450c3c94358166ceceec86df2701993eba0fa
Craig Treen <treencd@mcmaster.ca>
2021-07-06 09:22:51 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,106 @@
+import pygame as pg
+import random
+from os import path
+import time
+from sprites import *
+from settings import *
+from tilemap import *
+
+
+class Game:
+    def __init__(self):
+        # initialize game window
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        self.dt = 0.0
+        self.running = True
+        self.font_name = pg.font.match_font(FONT_NAME)
+        self.load_data()
+
+    def load_data(self):
+        game_folder = path.dirname(__file__)
+        self.map = Map(path.join(game_folder, 'maps/level1.csv'))
+
+    def new(self):
+        self.all_sprites = pg.sprite.Group()
+        self.map_tiles = pg.sprite.Group()
+        self.player = Player(self, WIDTH / 4, HEIGHT / 4)
+        self.all_sprites.add(self.player)
+        for row, tiles in enumerate(self.map.data):
+            # tiles = tiles.replace(',', '')
+            for col, tile in enumerate(tiles):
+                if tile == '1':
+                    map_tile = Tile(self, col, row, GREEN)
+                elif tile == '2':
+                    map_tile = Tile(self, col, row, BLUE)
+                # if tile == 'P':
+                #     self.player = Player(self, col, row)
+        self.camera = Camera(self.map.width, self.map.height)
+        self.run()
+
+    def run(self):
+        # Game Loop
+        self.playing = True
+        prev_time = 0
+        while self.playing:
+            # Compute delta time
+            now = time.time()
+            self.dt = (now - prev_time) * 100
+            prev_time = now
+            print(self.dt)
+            # Set FPS
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        # Game Loop - Update
+        self.all_sprites.update()
+        self.camera.update(self.player)
+        # spawn monsters
+
+        # check monster hits
+
+
+    def events(self):
+        # Game Loop - events
+        for event in pg.event.get():
+            # check for closing window
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.player.jump()
+
+    def draw(self):
+        # Game Loop - draw
+        self.screen.fill(BLACK)
+        for sprite in self.all_sprites:
+            self.screen.blit(sprite.image, self.camera.apply(sprite))
+        # self.map_tiles.draw(self.screen)
+        # self.player.draw(self.screen)
+        # *after* drawing everything, flip the display?
+        pg.display.flip()
+
+    def show_start_screen(self):
+        # game splash/start screen
+        pass
+
+    def show_go_screen(self):
+        # game over/continue
+        pass
+
+
+g = Game()
+g.show_start_screen()
+while g.running:
+    g.new()
+    g.show_go_screen()
+
+pg.quit()
---
+++ sprites.py
@@ -0,0 +1,107 @@
+import pygame as pg
+from settings import *
+vec = pg.math.Vector2
+
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        pg.sprite.Sprite.__init__(self)
+        self.x = x
+        self.y = y
+        self.game = game
+        self.image = pg.Surface((30, 40))
+        self.image.fill(YELLOW)
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.rect = self.image.get_rect()
+        # self.rect.center = (WIDTH / 2, HEIGHT / 4)
+        self.pos = vec(self.x, self.y)
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
+        self.jumping = False
+        self.update_rects()
+
+    def update_rects(self):
+        self.x = self.rect.x
+        self.y = self.rect.y
+        self.rectTop = pg.Rect(self.x, self.y, self.width, 10)
+        self.rectLeft = pg.Rect(self.x, self.y + 10, 10, self.height - 20)
+        self.rectRight = pg.Rect(self.x + self.width - 10, self.y + 10, 10, self.height - 20)
+        self.rectBottom = pg.Rect(self.x, self.y + self.height - 10, self.width, 10)
+
+    def get_keys(self):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT]:
+            self.acc.x = -PLAYER_ACC
+        if keys[pg.K_RIGHT]:
+            self.acc.x = PLAYER_ACC
+
+        # for event in pg.event.get():
+        #     if event.type == pg.KEYDOWN:
+        #         if event.key == pg.K_SPACE:
+        #             self.jump()
+
+    def collide_with_walls(self):
+           #check for collision
+            for tile in self.game.map_tiles:
+                #check for collision in x direction
+                if tile.rect.colliderect(self.rect.x + self.vel.x, self.rect.y, self.width, self.height):
+                    if self.vel.x < 0:
+                        self.pos.x = tile.rect.right
+                    elif self.vel.x > 0:
+                        self.pos.x = tile.rect.left - self.width
+                #check for collision in y direction
+                if tile.rect.colliderect(self.rect.x, self.rect.y + self.vel.y, self.width, self.height):
+                    #check if below the ground i.e. jumping
+                    if self.vel.y < 0:
+                        self.pos.y = tile.rect.bottom + self.height
+                        self.vel.y = 0
+                    #check if above the ground i.e. falling
+                    elif self.vel.y >= 0:
+                        self.pos.y = tile.rect.top
+                        self.vel.y = 0
+            
+    
+    def jump(self):
+        # jump only if standing on a platform
+        self.jumping = False
+        self.rect.y += 1
+        hits = pg.sprite.spritecollide(self, self.game.map_tiles, False)
+        self.rect.y -= 1
+        if hits:
+            self.jumping = True
+            self.vel.y = -PLAYER_JUMP
+
+    def update(self):
+        self.acc = vec(0, PLAYER_GRAV)
+        self.get_keys()
+        # apply friction
+        self.acc.x += self.vel.x * PLAYER_FRICTION
+        # equations of motion
+        self.vel += self.acc
+        if abs(self.vel.x) < 0.1:
+            self.vel.x = 0
+        self.pos += self.vel + 0.5 * self.acc
+        self.collide_with_walls()
+        # Map boundaries
+        if self.pos.x > self.game.map.width - self.rect.w:
+            self.pos.x = self.game.map.width - self.rect.w
+        if self.pos.x < 0:
+            self.pos.x = 0
+        
+        self.rect.bottomleft = self.pos
+        self.update_rects()
+        
+
+class Tile(pg.sprite.Sprite):
+    def __init__(self, game, x, y, color):
+        self.groups = game.map_tiles, game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
